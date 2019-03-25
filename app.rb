a5484cbad6ab0ef02794ee23ba90a7a3b0aebc7e
Myriam Boran <brnmyriam45@googlemail.com>
2019-03-25 21:11:32 +0000

    staff page

--- app.rb
+++ app.rb
@@ -3,7 +3,12 @@ require( 'sinatra/contrib/all' )
 require_relative('controllers/bookings_controller')
 require_relative('controllers/members_controller')
 require_relative('controllers/sessions_controller')
+require_relative('controllers/users_controller')
 
 get '/' do
   erb( :index )
+end
+
+get '/staff' do
+  erb( :staff )
 end
